a91caa6a058fc66e26ba632d844182f036aacb2e
claramooon <claire.ross@me.com>
2026-04-09 13:10:40 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -393,6 +393,3 @@ export default function ComicMaker() {
 
 import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById("root")).render(<ComicMaker />);
-
-import ReactDOM from "react-dom/client";
-ReactDOM.createRoot(document.getElementById("root")).render(<ComicMaker />);
